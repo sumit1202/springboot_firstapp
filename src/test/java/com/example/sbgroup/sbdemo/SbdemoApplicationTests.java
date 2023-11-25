02111d801a4829a0dcb8fd377138b4af5351ba68
sumitkumar <sumitkumar1202@gmail.com>
2023-11-25 20:10:53 +0530

    commit1

---
+++ src/test/java/com/example/sbgroup/sbdemo/SbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbgroup.sbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
